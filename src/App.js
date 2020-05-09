863d54ddc53de5a39510abc56924d24ead498fac
Dustin <dustinchristians@gmail.com>
2020-05-09 15:13:25 -0400

    Added routes for the get, post, put and delete messages components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import GetMessages from './components/GetMessages';
+import PostMessages from './components/PostMessages';
+import PutMessages from './components/PutMessages';
+import DeleteMessages from './components/DeleteMessages';
 import './App.scss';
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/get-messages" component={GetMessages} />
+        <Route exact path="/post-messages" component={PostMessages} />
+        <Route exact path="/put-messages" component={PutMessages} />
+        <Route exact path="/delete-messages" component={DeleteMessages} />
         <Route component={NotFound} />
       </Switch>
     </div>
