f24738aeb1622475d378e89558b27216e016e840
Dustin <dustinchristians@gmail.com>
2020-06-02 05:44:49 -0400

    Added error handling and a default value to the TextInput.

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,46 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextInput = ({ name, label, onChange, placeholder, value, error, children }) => {
-  let wrapperClass = 'form-group';
-  if (error && error.length > 0) {
-    wrapperClass += ' has-error';
-  }
-
+const TextInput = ({
+  name,
+  label,
+  onChange,
+  placeholder,
+  value,
+  defaultValue,
+  apiErrors,
+  validationErrors,
+  reference,
+  children,
+}) => {
   return (
-    <div className={wrapperClass}>
-      <label htmlFor={name}>{label}</label>
-      <div className="field">
-        <input
-          type="text"
-          name={name}
-          className="form-control"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
-        {error && <div className="alert alert-danger">{error}</div>}
+    <>
+      <div className="form-group">
+        <label htmlFor={name}>{label}</label>
+        <div className="field">
+          <input
+            type="text"
+            name={name}
+            className="form-control"
+            placeholder={placeholder}
+            value={value || undefined}
+            defaultValue={defaultValue || undefined}
+            onChange={onChange}
+            ref={reference}
+          />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+      <div className="invalid-feedback">
+        {apiErrors && <span className="invalid-feedback">{apiErrors}</span>}
+        {validationErrors[name] && validationErrors[name].type === 'required' && (
+          <span className="invalid-feedback">This field is required</span>
+        )}
+      </div>
+    </>
   );
 };
 
 TextInput.defaultProps = {
+  onChange: null,
   label: '',
   placeholder: '',
   value: '',
-  error: '',
+  defaultValue: '',
+  apiErrors: '',
+  validationErrors: null,
+  reference: null,
   children: null,
 };
 
 TextInput.propTypes = {
   name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
   label: PropTypes.string,
   placeholder: PropTypes.string,
   value: PropTypes.string,
-  error: PropTypes.string,
+  defaultValue: PropTypes.string,
+  apiErrors: PropTypes.string,
+  validationErrors: PropTypes.shape({}),
+  reference: PropTypes.func,
   children: PropTypes.node,
 };
 
