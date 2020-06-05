a562590270222607dd427fbf03fcc98ac2caba7e
Dustin <dustinchristians@gmail.com>
2020-06-05 06:20:42 -0400

    Added a text input that allows editing and deleting text through an API.

---
+++ src/components/common/UpdateDeleteTextInput.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import TextInput from './TextInput';
+
+const EditableTextInput = ({ item, handleSave, handleDelete, onChange, saving, validation }) => {
+  const { register, handleSubmit, errors } = useForm();
+
+  return (
+    <TextInput
+      name={`editItem${item.id}`}
+      defaultValue={item.text}
+      reference={register(validation)}
+      validationErrors={errors}
+      onChange={onChange}
+    >
+      <button
+        type="submit"
+        onClick={handleSubmit(handleSave.bind(this, item.id))}
+        disabled={saving}
+        className="btn btn-primary"
+      >
+        {saving ? 'Saving...' : 'Save'}
+      </button>
+      <button
+        type="submit"
+        onClick={handleDelete.bind(this, item.id)}
+        disabled={saving}
+        className="btn btn-primary"
+      >
+        Delete
+      </button>
+    </TextInput>
+  );
+};
+
+EditableTextInput.defaultProps = {
+  validation: null,
+};
+
+EditableTextInput.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+  }).isRequired,
+  handleSave: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  saving: PropTypes.bool.isRequired,
+  validation: PropTypes.shape({}),
+};
+
+export default EditableTextInput;
